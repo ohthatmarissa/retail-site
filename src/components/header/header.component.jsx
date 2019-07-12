962c9684bc5e123a2fb81907a38d66d039fc15db
Marissa Perry <ohthatmarissa@gmail.com>
2019-07-12 16:44:07 -0700

    add header component -unfinished, stopped at crown

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {  } from '../../assets/crown.svg';
+import './header.styles/scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link to="/">
+
+        </Link>
+    </div>
+)
