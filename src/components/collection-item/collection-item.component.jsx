f5833a9ed3a61cf13b98827771b406c2b3c4171b
Student <example@example.com>
2019-07-18 15:44:09 -0700

    add add to cart button in collection-item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButon from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -12,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
+            <CustomButon inverted>Add to cart</CustomButon>
     </div>
 );
 
