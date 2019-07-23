f332a5de058b4ccb149c7ff12123fe57b15ffd8a
Student <example@example.com>
2019-07-23 13:31:33 -0700

    add collections-overview and routing for collection-preview to shop.component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,6 +3,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 
+
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Directory from '../../components/directory/directory.component';
 import './Homepage.styles.scss';
 
